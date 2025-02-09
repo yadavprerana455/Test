3640726b5ad9b3b6f1c607286f3f54f88b83a637
yadavprerana455 <yadavprerana455@gmail.com>
2025-02-09 15:22:45 +0530

    First commit

---
+++ src/main/java/com/example/demo/TestApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
+
+package com.example.demo.controller;
+
+public class homeController {
+
+	private void syso() {
+		// TODO Auto-generated method stub
+
+	}
+}
+
